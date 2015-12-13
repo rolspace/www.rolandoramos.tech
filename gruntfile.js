d13a22a04b62d5151fb3825d0061a2955864f01e
rolspace <rolandorramos@gmail.com>
2015-12-13 23:22:45 +0100

    Changed gruntfile to simplify tasks

--- gruntfile.js
+++ gruntfile.js
@@ -16,14 +16,6 @@ module.exports = function(grunt) {
 			main: {
 				files: [
 					{
-						src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
-						dest: 'css/bootstrap.min.css'
-					},
-					{
-						src: 'bower_components/font-awesome/css/font-awesome.min.css',
-						dest: 'css/font-awesome.min.css'
-					},
-					{
 						src: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
 						dest: 'js/bootstrap.min.js'
 					},
@@ -51,7 +43,10 @@ module.exports = function(grunt) {
 				separator: ' '
 			},
 			main: {
-				src: ['css/bootstrap.min.css', 'css/font-awesome.min.css', 'css/rolspace.css'],
+				src: ['bower_components/bootstrap/dist/css/bootstrap.min.css',
+					  'bower_components/font-awesome/css/font-awesome.min.css',
+					  'css/rolspace.css'
+					 ],
 				dest: 'css/rolspace.css'
 			}
 		},
@@ -66,11 +61,12 @@ module.exports = function(grunt) {
 			}
 		},
 		shell: {
-			demo: {
-				command: 'jekyll serve --config _config.debug.yml --force'
-			},
-			release: {
-				command: 'jekyll serve --config _config.yml --force'
+			main: {
+				command: function(demo) {
+
+
+					return 'jekyll serve --config _config' + demo + '.yml --force'
+				}
 			}
 		},
 		watch: {
@@ -80,17 +76,20 @@ module.exports = function(grunt) {
 			},
 			files: ['_less/*.less', '_assets/*.*', '_includes/*.*', '_layouts/*.*',
 					'_posts/*.*', 'about/*.*', 'read/*.*', 'index.html'],
-			tasks: ['demo']
+			tasks: ['less', 'copy', 'concat', 'shell:main:.demo']
 		}
 	});
 
-	grunt.registerTask('start', 'Start "demo" task with "grunt watch"', function() {
-		grunt.task.run('watch');
+	grunt.registerTask('start', 'Start the website', function() {
+		//Include the --demo option to start
+		//the demo website with the "watch" task
+		if (grunt.option('demo')) {
+			grunt.task.run('watch');
+		}
+
+		grunt.task.run('release');
 	});
 
-	grunt.registerTask('demo', 'Deploy demo website using "jekyll serve"',
-		['less', 'copy', 'concat', 'shell:demo']);
-
-	grunt.registerTask('release', 'Deploy release website using "jekyll serve"',
-		['less', 'copy', 'concat', 'cssmin', 'shell:release']);
+	grunt.registerTask('release', 'Start the release version of the website',
+		['less', 'copy', 'concat', 'cssmin', 'shell:main:']);
 }

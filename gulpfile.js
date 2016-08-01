4a09b7152adac75501b9cf220d10bed7b84e40a2
rolspace <rolandorramos@gmail.com>
2016-08-01 17:10:49 +0200

    Modified 'less' task and added support for concatenation and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
+'use strict';
+
 var path = require('path');
 
-var gless = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
 var gulp = require('gulp');
 
-var css = {
-	less: function() {
+var config = {
+	css: {
+		bootstrap: './bower_components/bootstrap/dist/css/bootstrap.min.css',
+		fontAwesome: './bower_components/font-awesome/css/font-awesome.min.css'
+	}
+};
 
-		return gulp.src(['./_less/default/rolspace.less'])
-			.pipe(gless({
+var css = {
+	concat: function() {
+		return gulp.src([config.css.bootstrap, config.css.fontAwesome, './dist/css/rolspace.css'])
+			.pipe(sourcemaps.init())
+			.pipe(concat('rolspace.css'))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest('./dist/css'));
+	},
+	less: function() {
+		return gulp.src(['./_less/default/*.less'])
+			.pipe(less({
 				filename: 'rolspace.css',
 				paths: [ './_less/default/includes' ]
 			}))
@@ -16,5 +33,6 @@ var css = {
 };
 
 gulp.task('css:less', css.less);
+gulp.task('css:concat', ['css:less'], css.concat);
 
-gulp.task('demo', ['css:less']);
+gulp.task('demo', ['css:concat']);

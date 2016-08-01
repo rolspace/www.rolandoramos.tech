89800683e003db47f8de921186e42785aae89575
rolspace <rolandorramos@gmail.com>
2016-08-01 03:04:48 +0200

    Added additional changes to the 'css:less' task using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
-var less = require('gulp-less');
+var path = require('path');
+
+var gless = require('gulp-less');
 var gulp = require('gulp');
 
 var css = {
 	less: function() {
-		return gulp.src(['./_less/default/rolspace.less']).
-			pipe(less({
-				filename: 'rolspace.css',
-				paths: [ './less/default' ]
-			})).
-			pipe(gulp.dest('./dist/css/'));
-	}
-}
 
-gulp.task('demo', function() {
-	css.less();
-});
+		return gulp.src(['./_less/default/rolspace.less'])
+			.pipe(gless({
+				filename: 'rolspace.css',
+				paths: [ './_less/default/includes' ]
+			}))
+			.pipe(gulp.dest('./dist/css/'));
+	}
+};
+
+gulp.task('css:less', css.less);
+
+gulp.task('demo', ['css:less']);

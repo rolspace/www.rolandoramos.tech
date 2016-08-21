745ba837b9d65b282892291b8785ba13b1151c60
rolspace <rolandorramos@gmail.com>
2016-08-21 20:32:14 +0200

    Various updates to gulpfile. Included js copy task and js eslint task. Added eslint to dependency list.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var path = require('path');
 var runSequence = require('run-sequence');
 
 var cleanCss = require('gulp-clean-css');
@@ -9,14 +8,18 @@ var autoprefixer = require('gulp-autoprefixer');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
 var less = require('gulp-less');
-var gzip = require('gulp-gzip');
 var gulp = require('gulp');
 
 var config = {
 	css: {
 		bootstrap: './bower_components/bootstrap/dist/css/bootstrap.min.css',
 		fontAwesome: './bower_components/font-awesome/css/font-awesome.min.css'
+	},
+	js: {
+		jquery: 'bower_components/jquery/dist/jquery.min.js',
+		bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min.js'
 	}
 };
 
@@ -37,22 +40,41 @@ var css = {
 			}))
 			.pipe(gulp.dest('./dist/css/'));
 	},
-	minify: function() {
-		return gulp.src(['./dist/css/rolspace.css'])
+	minify: function(callback) {
+		gulp.src(['./dist/css/rolspace.css'])
 			.pipe(replace(/\/*# sourceMappingURL[^\n]*/g, ''))
 			.pipe(sourceMaps.init())
 			.pipe(cleanCss())
 			.pipe(rename('rolspace.min.css'))
 			.pipe(sourceMaps.write('./'))
 			.pipe(gulp.dest('./dist/css/'));
+		callback();
+	}
+};
+
+var js = {
+	concat: function() {
+		return gulp.src([config.js.jquery, config.js.bootstrap, './_scripts/ui-setup.js', './_scripts/main.js'])
+			.pipe(concat('rolspace.js'))
+			.pipe(gulp.dest('./dist/js'));
+	},
+	lint: function(callback) {
+		gulp.src(['./_scripts/rolspace.js', './gulpfile.js'])
+			.pipe(eslint())
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError());
+		callback();
 	}
 };
 
 gulp.task('css:less', css.less);
 gulp.task('css:concat', css.concat);
 gulp.task('css:minify', css.minify);
+gulp.task('css', function(callback) { runSequence('css:less', 'css:concat', 'css:minify', callback); });
 
-gulp.task('css', runSequence('css:less', 'css:concat', 'css:minify'));
+gulp.task('js:concat', js.concat);
+gulp.task('js:lint', js.lint);
+gulp.task('js', function(callback) { runSequence('js:concat', 'js:lint', callback); });
 
 gulp.task('dev', ['css']);
 //gulp.task('prod');

3fc74cd5d53aaa059df256f36bc4e6de1e627c1e
rolspace <rolandorramos@gmail.com>
2017-03-05 23:56:58 +0100

    Moved external method into class code

--- _scripts/v1/setup.js
+++ _scripts/v1/setup.js
@@ -1,14 +1,13 @@
-function activateMenu() {
-	'use strict';
-	$('a[href="' + window.location.pathname + '"]').parents('li').addClass('active');
-}
-
 export class Setup {
 	constructor() {
 
 	}
 
 	init() {
-		activateMenu();
+		this.activateMenu();
+	}
+
+	activateMenu() {
+		$('a[href="' + window.location.pathname + '"]').parents('li').addClass('active');
 	}
 }

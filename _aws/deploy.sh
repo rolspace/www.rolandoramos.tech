a03c4cab0e261a4c416987789d62d60e6410a35c
rolspace <rolandorramos@gmail.com>
2017-10-13 23:42:45 +0200

    Added shebang to deploy script

--- _aws/deploy.sh
+++ _aws/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 aws s3 cp ../site/dist/css/site.min.css s3://bucketname/dist/css/ --cache-control "public, max-age=2800000" --content-encoding gzip
 
 aws s3 cp ../site/dist/js/site.min.js s3://bucketname/dist/js/ --cache-control "public, max-age=2800000" --content-encoding gzip

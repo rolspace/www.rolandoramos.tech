5d328779fb1cde7bb902c93a14f54faba11e5e4a
rolspace <rolandorramos@gmail.com>
2019-04-13 14:54:51 +0200

    Added global styles component

---
+++ rebuild/src/styles/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: #3c3c3c;
+    font-size: 20px;
+    font-family: Amiri;
+  }
+`
+
+export default GlobalStyle

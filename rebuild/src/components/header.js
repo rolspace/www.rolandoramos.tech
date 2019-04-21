cae53dea299159df24ed8f99b977a406c0108bda
rolspace <rolandorramos@gmail.com>
2019-04-21 13:16:30 +0200

    Continued adding styling for the website header

--- rebuild/src/components/header.js
+++ rebuild/src/components/header.js
@@ -5,15 +5,9 @@ import styled from "styled-components"
 
 import logo from "../../content/assets/logo.png"
 
-const Wrapper = styled.header`
-  line-height: 1.5;`
-
 const Logo = styled.h1`
-  margin: 0;  
-  padding: 0 15px;`
-
-const StyledBox = styled(Box)`
-  display: flex;
+  line-height: 1;
+  margin: 0;
 `
 
 const Menu = styled.div`
@@ -22,27 +16,33 @@ const Menu = styled.div`
 `
 
 const MenuLink = styled(Link)`
+  color: #adadad;
+  font-family: Poppins;
+  font-size: .65em;
+  font-weight: 500;
+  letter-spacing: .03em;
+  padding: 0.3rem 0.5rem;
+  text-transform: uppercase;
+  text-decoration: none;
 `
 
 const Header = (props) => {
   return (
-    <Wrapper>
-      <Flex flexWrap="wrap">
-        <Box width={[1, 1/2, 2/3]}>
-          <Logo>
-            <Link to={`/`}>
-              <img src={logo} width={225} alt="www.rolandoramos.tech" />
-            </Link>
-          </Logo>
-        </Box>
-        <StyledBox width={[1, 1/2, 1/3]}>
-          <Menu>
-            <MenuLink to={`/posts/`}>Posts</MenuLink>
-            <MenuLink to={`/about/`}>About</MenuLink>
-          </Menu>
-        </StyledBox>
-      </Flex>
-    </Wrapper>
+    <Flex as="header" flexDirection="row" flexWrap="nowrap" alignItems="center">
+      <Box width={[1, 1/2, 2/3]}>
+        <Logo>
+          <Link style={{ display: `inline-block` }} to={`/`}>
+            <img style={{ marginLeft: `-0.5rem`, verticalAlign: `middle` }} src={logo} width={225} alt="www.rolandoramos.tech" />
+          </Link>
+        </Logo>
+      </Box>
+      <Box style={{ display: `flex` }} width={[1, 1/2, 1/3]}>
+        <Menu>
+          <MenuLink to={`/posts/`}>Posts</MenuLink>
+          <MenuLink to={`/about/`}>About</MenuLink>
+        </Menu>
+      </Box>
+    </Flex>
   )
 }
 
--- rebuild/src/components/layout.js
+++ rebuild/src/components/layout.js
@@ -1,8 +1,19 @@
 import React, { Fragment } from "react"
+import styled from "styled-components"
 import { Flex, Box } from "@rebass/grid"
 
 import GlobalStyle from "../styles/global-styles"
 import Header from "./header"
+
+const HeaderBox = styled(Box)`
+  background-color: #fff;
+  box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.2);
+  line-height: 1.5;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  position: sticky;
+  top: 0;
+  z-index: 1000;`
 
 class Layout extends React.Component {
   render() {
@@ -11,10 +22,10 @@ class Layout extends React.Component {
     return (
       <Fragment>
         <GlobalStyle />
-        <Flex flexWrap='wrap' alignItems='center'>
-          <Box width={1}>
+        <Flex flexDirection="column" flexWrap='nowrap' alignItems='center'>
+          <HeaderBox width={1}>
             <Header />
-          </Box>
+          </HeaderBox>
           <Box width={[1, 1/2]} mx='auto'>
             <main>{children}</main>
           </Box>

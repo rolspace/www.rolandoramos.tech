87e2182c069eb7fd52c036e1625e4a3606e04e95
rolspace <rolandorramos@gmail.com>
2019-07-27 00:14:17 +0200

    Corrected styles for links used in the pager

--- rebuild/src/components/styled/page-link.js
+++ rebuild/src/components/styled/page-link.js
@@ -4,13 +4,17 @@ import styled from 'styled-components'
 const PageLink = styled(Link)`
   color: #888;
   font-family: Poppins;
-  font-size: 0.75rem;
+  font-size: 0.70rem;
   font-weight: 400;
   letter-spacing: 0.05em;
   text-decoration: none;
 
   &:hover {
     color: #555;
+  }
+
+  & svg {
+    vertical-align: middle;
   }`
 
 export default PageLink

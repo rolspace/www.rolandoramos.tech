93f76a5d39610de6764a664303075d247e40d775
rolspace <rolandorramos@gmail.com>
2019-10-17 17:50:32 +0200

    Added styles for code highlighting and html tables

--- rebuild/src/styles/global-styles.js
+++ rebuild/src/styles/global-styles.js
@@ -15,8 +15,47 @@ const GlobalStyle = createGlobalStyle`
     touch-action: manipulation;
   }
 
+  code {
+    background-color: #f7f7f9;
+    border-radius: .25rem;
+    color: #bd4147;
+    font-size: 90%;
+    padding: .2rem .4rem;
+  }
+
+  article {
+    .subtitle {
+      color: #3c3c3c;
+      display: block;
+      font-family: Poppins;
+      font-size: 0.90em;
+      font-weight: 500;
+    }
+
+    .table-responsive {
+      width: 100%;
+    }
+  }
+
+  table {
+    border-collapse: collapse;
+    font-family: Poppins;
+    font-size: 0.85em;
+    width: 100%;
+
+    th, tr, td {
+      border: 1px solid #ccc;
+      padding: 0.4rem 0.5rem;
+    }
+
+    th {
+      background-color: #e1e1e1;
+      text-align: left;
+    }
+  }
+
   .gatsby-highlight {
-    font-size: 0.8rem;
+    font-size: 0.80em;
   }
 `
 

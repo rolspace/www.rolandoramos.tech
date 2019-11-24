b234d576f9b5223e016496fc2ecdddb7393e5e4b
rolspace <rolandorramos@gmail.com>
2019-11-24 15:07:35 +0100

    Refactored gatsby-node createPages implementation

--- rebuild/gatsby-node.js
+++ rebuild/gatsby-node.js
@@ -1,12 +1,70 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
+const createBlogIndexPages = (createPage, posts) => {
+  // Create blog index pages
+  const postsPerPage = 5
+  const pageCount = Math.ceil(posts.length / postsPerPage)
+  const blogIndexTemplate = path.resolve('./src/templates/blog-index.js')
+
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? '/' : `/page/${i + 1}`,
+      component: blogIndexTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        pageCount,
+        currentPage: i + 1,
+      },
+    })
+  })
+}
+
+const createBlogTitlesPage = (createPage) => {
+  // Create blog titles page
+  const blogTitlesTemplate = path.resolve('./src/templates/blog-titles.js')
+
+  createPage({
+    path: '/posts',
+    component: blogTitlesTemplate,
+  })
+}
+
+const createBlogPostPages = (createPage, posts) => {
+  // Create blog posts pages.
+  const blogPostTemplate = path.resolve('./src/templates/blog-post.js')
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+}
+
+const createAboutPage = (createPage) => {
+  const blogAboutTemplate = path.resolve('./src/templates/blog-about.js')
+
+  createPage({
+    path: '/about',
+    component: blogAboutTemplate,
+  })
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const firstPromise = graphql(
-    `
-      {
+  const blogQuery = await graphql(
+    `{
         allMarkdownRemark(
           filter: { fileAbsolutePath: { regex: "/blog/" }}
           sort: { fields: [frontmatter___date], order: DESC }
@@ -24,100 +82,17 @@ exports.createPages = async ({ graphql, actions }) => {
             }
           }
         }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+      }`
+  )
 
-    const posts = result.data.allMarkdownRemark.edges
+  const posts = blogQuery.data.allMarkdownRemark.edges
 
-    // Create blog index pages
-    const postsPerPage = 5
-    const pageCount = Math.ceil(posts.length / postsPerPage)
-    const blogIndexTemplate = path.resolve('./src/templates/blog-index.js')
+  createBlogIndexPages(createPage, posts)
+  createBlogTitlesPage(createPage)
+  createBlogPostPages(createPage, posts)
+  createAboutPage(createPage)
 
-    Array.from({ length: pageCount }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? '/' : `/page/${i + 1}`,
-        component: blogIndexTemplate,
-        context: {
-          limit: postsPerPage,
-          skip: i * postsPerPage,
-          pageCount,
-          currentPage: i + 1,
-        },
-      })
-    })
-
-    // Create blog titles page
-    const blogTitlesTemplate = path.resolve('./src/templates/blog-titles.js')
-
-    createPage({
-      path: '/posts',
-      component: blogTitlesTemplate,
-    })
-
-    // Create blog posts pages.
-    const blogPostTemplate = path.resolve('./src/templates/blog-post.js')
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPostTemplate,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-
-    return null
-  })
-
-  const secondPromise = graphql(
-    `
-      {
-        allMarkdownRemark(
-          filter: { fileAbsolutePath: { regex: "/about/" }}
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fileAbsolutePath
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
-
-    const blogAboutTemplate = path.resolve('./src/templates/blog-about.js')
-
-    createPage({
-      path: '/about',
-      component: blogAboutTemplate,
-    })
-
-    return null
-  })
-
-  await Promise.all([firstPromise, secondPromise])
+  return null
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {

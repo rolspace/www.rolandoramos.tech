b03324afc99e32355e12866f5b3d7a3efdfcb80d
rolspace <rolandorramos@gmail.com>
2019-12-22 11:15:49 +0100

    Deleted old website files

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-gem 'jekyll'
-gem 'jekyll-paginate', group: :jekyll_plugins
-gem 'kramdown', group: :jekyll_plugins
-gem 'pygments.rb', group: :jekyll_plugins
--- _scripts/setup.js
+++
@@ -1,9 +0,0 @@
-export default class Setup {
-  init() {
-    this.activateMenu();
-  }
-
-  activateMenu() {
-    $('.navbar a.nav-item[href="' + window.location.pathname + '"]').addClass('active');
-  }
-}

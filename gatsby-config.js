b6e91f5fc4a1ba4c58fbbd15862f7cf1ef20ddf6
rolspace <rolandorramos@gmail.com>
2019-12-25 17:14:07 +0100

    Installed and configured s3 plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,91 @@
 module.exports = {
   siteMetadata: {
-    title: `Rolando Ramos | technology lead. problem solver. mentor.`,
-    author: `Rolando Ramos`,
-    description: `My name is Rolando Ramos, welcome to my personal website.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: 'Rolando Ramos | technology lead. problem solver. mentor.',
+    author: 'Rolando Ramos',
+    description: 'My name is Rolando Ramos, welcome to my personal website.',
+    siteUrl: 'https://www.rolandoramos.tech/',
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: 'www.rolspace.com',
+        protocol: 'https',
+        hostname: 'www.rolandoramos.tech',
+      },
+    },
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/about`,
-        name: `about`,
+        name: 'about',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
+        name: 'assets',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         excerpt_separator: '<!--more-->',
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+              wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
               noInlineHighlight: true,
             },
           },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        // trackingId: 'ADD YOUR TRACKING ID HERE',
       },
     },
-    `gatsby-plugin-feed`,
+    'gatsby-plugin-feed',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Rolando Ramos | technology lead. problem solver. mentor.`,
-        short_name: `Rolando Ramos`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/icon.png`,
+        name: 'Rolando Ramos | technology lead. problem solver. mentor.',
+        short_name: 'Rolando Ramos',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'content/assets/icon.png',
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
   ],
 }
